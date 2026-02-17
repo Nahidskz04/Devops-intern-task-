2a725cb966b8bd6ebce2004e969ad4d80f9fdf6c
Nahid Javed Shaikh <164649096+Nahidskz04@users.noreply.github.com>
2026-02-17 17:26:29 +0530

    Change greeting message in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return "Hello from Nahid Shaikh\n"
+    return "Hello from devops intern\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
