50a858bb9a831bca3596246929c40dcaa5146bec
Nahid Javed Shaikh <164649096+Nahidskz04@users.noreply.github.com>
2026-02-17 17:24:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return "Hello from DevOps Intern\n"
+    return "Hello from Nahid Shaikh\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
